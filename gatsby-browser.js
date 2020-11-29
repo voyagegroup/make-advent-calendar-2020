81a351ecddc314aece465a9f9a8f3cc3ec303fab
tamakiii <tamakiii@users.noreply.github.com>
2020-11-29 13:08:05 +0900

    using prism-material-light.css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,4 @@
 
 // You can delete this file if you're not using it
 import "./src/css/index.css"
-import "prismjs/themes/prism-solarizedlight.css"
+import "prism-themes/themes/prism-material-light.css"
