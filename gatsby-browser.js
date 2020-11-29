b0fd60b6f45d36250771c82df85ad9b958311708
tamakiii <tamakiii@users.noreply.github.com>
2020-11-29 18:01:38 +0900

    fix-bbb

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,4 @@
 
 // You can delete this file if you're not using it
 import "./src/css/index.css"
-import "prism-themes/themes/prism-material-light.css"
+import "prism-themes/themes/prism-duotone-light.css"
