71471b0f0cf6a2aaf8b7afc47d3d64719ccac94b
tamakiii <tamakiii@users.noreply.github.com>
2020-11-30 15:41:23 +0900

    setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,15 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-K29D3L3XGS',
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-react-helmet`,
     },
     {
